2438def6726b4859ab57ac51a74c31fefbd4e37a
dev-sreelekshmi-pillai <sree0008lakshmi@gmail.com>
2024-11-06 12:23:27 +0530

    HTML page is created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { task } from './model/task.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to_do_app';
-}
+  tagsList=['hyhy','sdfdf']
+  public task:task[]=[
+    {
+      itemId: 1,
+      taskName: 'sryetydff',
+      taskDescription: '',
+      dueDate: new Date,
+      isCompleted: false,
+      tags: ['asdas']
+    },
+    {
+      itemId: 2,
+      taskName: 'cbdhdhb',
+      taskDescription: '',
+      dueDate: new Date,
+      isCompleted: false,
+      tags: ['asdas','sdfaf']
+    },
+    {
+      itemId: 3,
+      taskName: 'dhgjcxgnfhx',
+      taskDescription: '',
+      dueDate: new Date,
+      isCompleted: true,
+      tags: ['asdas']
+    }
+  ]
+  
+  }
+
